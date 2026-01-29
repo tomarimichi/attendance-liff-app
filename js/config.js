ef088e650fefad3e8cba69f33166c03fbbac4575
tomarimichi <tomarimichi@gmail.com>
2026-01-29 09:59:11 +0900

    0959

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // ================================
 // 設定
 // ================================
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbwv3fTjFwZJ5CXLhdNaqA2u0fXTLvG9386t5MhrRmUjBQfDmWsqsuKlDbz8JNkCM7I/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbyHsc7nqCA2IM2Cm4hZoA0uuaxbN95QA-N1xKuuBf9ryT8fedCpJzN4sna7rK8F5_mk/exec';
 const LIFF_ID = '2008783538-yHgAa1tC';
