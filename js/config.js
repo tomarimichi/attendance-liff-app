4524b5f3438fd5a90f86c21a343ea0aa960863b9
tomarimichi <tomarimichi@gmail.com>
2026-01-29 11:02:56 +0900

    1102

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // ================================
 // 設定
 // ================================
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbz6qTflLDRfR5Uh9mF7ANWIzsVLYOvI7eHgdDJb_tDPvXTVvHIx1Ieth8yp6-aI8ySp/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbzDQbqnA1XDNwPzXSZzTOURN0KPe1QkPJKEGbg02eSoPyVysixLfsUW6Az98WRjfZ2Z/exec';
 const LIFF_ID = '2008783538-yHgAa1tC';
