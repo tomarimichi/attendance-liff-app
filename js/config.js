43a5d85f03b6f965e48254207c5fdad42f60ffc3
tomarimichi <tomarimichi@gmail.com>
2026-02-06 14:30:31 +0900

    1230

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // 設定
 // ================================
 const GAS_ID = 
-'AKfycbymiPJCZYb2McuB-rSk-TL4Q0see55cQYnBXa9z2vNpu2pooliY1xmfM25r8GhtIJ6V';
+'AKfycbw9sXJxKjhJvwoOO3JhfA6OIMKYCMGtHbKuMo0b3pqKo74tOGkgWDxdxT3Q_rUTTFKU';
 
 const GAS_URL = `https://script.google.com/macros/s/${GAS_ID}/exec`;
 
