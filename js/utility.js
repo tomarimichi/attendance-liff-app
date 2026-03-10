c9e2781f2c54833c16d6732a8d629f63f1a80931
tomarimichi <tomarimichi@gmail.com>
2026-03-10 13:05:04 +0900

    1304

--- js/utility.js
+++ js/utility.js
@@ -190,7 +190,7 @@ async function fetchWithTimeout(promise, timeout = 30000) {
   }, timeout);
 
   try {
-    const res = await Promise({ signal: controller.signal });
+    const res = await new Promise({ signal: controller.signal });
     return res;
   } catch (err) {
     if (err.name === 'AbortError') {
