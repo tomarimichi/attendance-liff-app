2d9463cec3962bde6e379deca6ccfae2498faffc
tomarimichi <tomarimichi@gmail.com>
2026-03-12 13:57:15 +0900

    Update index.html for production: comment out test parts, switch to config_prob.js

---
+++ js/config_prob.js
@@ -0,0 +1,16 @@
+// ================================
+// 設定
+// ================================
+const GAS_ID = 
+'AKfycbxZvubfezFgESs4oDYdiGS6In4kDtxSSWjrjwGevpyju-kx58WxjfqOGbXX47_DSiyO';
+
+const GAS_URL = `https://script.google.com/macros/s/${GAS_ID}/exec`;
+
+const LIFF_ID = '2008783538-yHgAa1tC';
+
+// ✅ 固定キー
+const MASTER_DATA_KEY = 'masters';
+const MASTER_VERSION_KEY = 'masterVersion';
+
+// ランダムID
+const submissionId = crypto.randomUUID();
