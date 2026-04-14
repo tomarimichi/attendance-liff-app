7a4efe10ff09135917dfeba0ea27b60a7ecd0c42
tomarimichi <tomarimichi@gmail.com>
2026-04-14 14:56:28 +0900

    1456

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@ const CONFIG = {
         dev: {
             LIFF_ID: "2008783538-KeEB2k47",
             GAS_ID:
-                'AKfycbzSheIJvqrf1XIuKeLw9xsyU1oC__Nm2Rc1ABNMwnopq1jTE-ZxVLdF_20K9TEKAlvTAg'
+                'AKfycbxXwlqA-EDwu229HDkMHBEFJnCyv6gj6__8UJrYu_dKtOPF_OarX0LWedJb-AporJ3G-w'
         },
         prod: {
             LIFF_ID: '2008783538-yHgAa1tC',
