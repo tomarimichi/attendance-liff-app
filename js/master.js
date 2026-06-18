9cb72944dec7a4112af3599d2a882271de405292
tomarimichi <tomarimichi@gmail.com>
2026-06-18 11:15:08 +0900

    11:15

--- js/master.js
+++ js/master.js
@@ -264,7 +264,7 @@ function addDays(date, days) {
 }
 
 function formatDate(date) {
-  const y = date.getfullYear();
+  const y = date.getFullYear();
   const m = String(date.getMonth()+1).padStart(2,`0`);
   const d = String(date.getDate()).padStart(2,`0`);
   
